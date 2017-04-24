47c9a8844cfd8ed0f1706a3721c3c2e2cbbb5072
Anna Henryson <gushenanm@student.gu.se>
2017-04-24 12:57:22 +0200

    changes in the ui

---
+++ BluetoothRemote/app/src/main/gen/se/gu/dit524/group5/bluetoothremote/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package se.gu.dit524.group5.bluetoothremote;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
