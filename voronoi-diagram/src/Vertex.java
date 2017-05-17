d1af40151715fa4d88198bc2a24a9ca0a729ba16
MGhanem <gusghanemo@student.gu.se>
2017-05-17 14:49:24 +0200

    Progress so far with stuff

---
+++ voronoi-diagram/src/Vertex.java
@@ -0,0 +1,31 @@
+/**
+ * Created by mghan on 2017-05-17.
+ */
+public class Vertex {
+    int x;
+    int y;
+    String identifier;
+
+    public Vertex(int x, int y, String indentifier)
+    {
+        this.x = x;
+        this.y = y;
+        this.identifier = identifier;
+    }
+
+
+
+
+    /**
+     * Get string of Vertex with all it's ingoing and outgoing adjacencies
+     * @ return string
+     */
+    @Override
+    public String toString()
+    {
+        return null;
+    }
+
+
+}
+
---
+++ voronoi-diagram/src/main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by mghan on 2017-05-17.
+ */
+public class main {
+    public static void main(String[] args){
+
+        /* Function demo
+        Vertex v1 = new Vertex(3,4, "V1");
+        Vertex v2 = new Vertex(10, 43, "V2");
+        LinearFunction function = new LinearFunction();
+        String func = function.getLinearFunction(v1,v2);
+        System.out.println(func);
+        */
+    }
+}
