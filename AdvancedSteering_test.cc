b0a1c6e337ff5fcb2610039089e4965058662d45
Simon <gussheqi@student.gu.se>
2017-05-19 13:14:45 +0200

    Tried some changes but not complete

--- AdvancedSteering_test.cc
+++ AdvancedSteering_test.cc
@@ -6,53 +6,64 @@
 
 #include "../../src/AdvancedSteering.ino" // Our production code
 
-using ::testing::_;
-// is this needed?
-using ::testing::Return;
+using ::testing::_;   
+using ::testing::Return;  // is this needed?
 
 class AdvancedSteeringFixture : public ::testing::Test
 {
 public:
     ArduinoMock* arduinoMock; // Necessary for delay()
     SerialMock* serialMock;
-    AdvancedSteeringMock* AdvancedSteering_mock;
+    GP2Y0A21Mock* GP2Y0A21_mock;
+    OdometerMock* odometerMock;
+    GyroscopeMock* gyroscopeMock;
+    CarMock* carMock;
+    //ServoMock* servoMock;
+    SR04_Mock* SR04_mock
     // Run this before the tests
     virtual void SetUp()
     {
         arduinoMock = arduinoMockInstance();
         serialMock = serialMockInstance();
-        AdvancedSteering_mock = AdvancedSteeringMockInstance();
-	GP2D120_mock = GP2D120MockInstance();
-	odometerMock = odometerMockInstance();
-	gyroscopeMock = gyroscopeMockInstance();
+	    GP2Y0A21_mock = GP2Y0A21MockInstance();
+	    odometerMock = odometerMockInstance();
+	    gyroscopeMock = gyroscopeMockInstance();
+        carMock = carMockInstance();
+        //servoMock = servoMockInstance();
+        SR04_mock = SR04MockInstance();
     }
     // Run this after the tests
     virtual void TearDown()
     {
         releaseArduinoMock();
         releaseSerialMock();
-        releaseAdvancedSteeringMock();
-	releaseGP2D120Mock();
-	releaseOdometerMock();
-	releaseGyroscopeMock();
+        releaseCarMock();
+	    releaseGP2Y0A21Mock();
+	    releaseOdometerMock();
+	    releaseGyroscopeMock();
+        //releaseServoMock();
+        releaseSR04_Mock();
 
     }
 };
 
 TEST_F(AdvancedSteeringFixture, initsAreCalled) {
     EXPECT_CALL(*serialMock, begin(_));
-    EXPECT_CALL(*AdvancedSteering_mock, attach(TRIGGER_PIN, ECHO_PIN));
-    EXPECT_CALL(*GP2D120_mock, attach(SIDE_FRONT_PIN));
+    //EXPECT_CALL(*AdvancedSteering_mock, attach(TRIGGER_PIN, ECHO_PIN));
+    EXPECT_CALL(*GP2Y0A21_mock, attach(SIDE_FRONT_PIN));
     EXPECT_CALL(*odometerMock, attach(encoderPin));
     EXPECT_CALL(*odometerMock, begin());
+    EXPECT_CALL(*gyroscopeMock, attach());
     EXPECT_CALL(*gyroscopeMock, begin(_));
+    EXPECT_CALL(*carMock,begin());
+    EXPECT_CALL(*SR04_mock,attach(TRIGGER_PIN,ECHO_PIN));
     setup();
 }
 
 TEST_F(AdvancedSteeringFixture, expectGetDistanceCall) {
     EXPECT_CALL(*AdvancedSteering_mock, getMedianDistance(_));
     EXPECT_CALL(*odometerMock, getDistance());
-    EXPECT_CALL(*GP2D120_mock, getDistance());
+    EXPECT_CALL(*GP2Y0A21_mock, getDistance());
     loop();
 }
 
